a7ba510dc1daf8b4df6834dc2b4ae5b4840095ea
Celebdil <lego-T.A.K@web.de>
2025-05-12 12:15:29 +0200

    Task 4

--- src/main/java/com/payroll/service/impl/CalculationServiceImpl.java
+++ src/main/java/com/payroll/service/impl/CalculationServiceImpl.java
@@ -176,7 +176,14 @@ public class CalculationServiceImpl implements CalculationService {
     }
 
     private static double getTaxFactor(TaxClass taxClass) {
-        return 1.0;// TODO: Implement tax factor calculation
+        /*
+        This works although it would be cleaner to dynamically
+        fetch the tax class to tax rate matching tax_class_data.csv
+         */
+        if (taxClass.factor() == 1.0) return 0.8;
+        else if (taxClass.factor() == 3.0) return 0.9;
+        else if (taxClass.factor() == 4.0) return 0.85;
+        return 1.0;
     }
 
 
